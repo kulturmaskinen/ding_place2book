97cbd1fd23e70b48ccc50564c8672f016ba3bef0
Tobias F. Petersen <tobsf@hotmail.com>
2016-09-07 12:12:30 +0200

    add cog pre-loader ( fontawesome)

--- js/ding_place2book.js
+++ js/ding_place2book.js
@@ -6,9 +6,10 @@
 jQuery(document).ready(function($) {
 	$('.place2book-ticketinfo').each(function() { 
 	    var obj = this;
-            $(obj).replaceWith("<div class='p2b_preloader'><i class='fa fa-cog fa-spin fa-2x fa-fw'></i><div>");
+            $(obj).parent().append("<div class='p2b_preloader'><i class='fa fa-cog fa-spin fa-2x fa-fw'></i><div>");
 		$.getJSON(Drupal.settings.basePath + 'ding/place2book/ticketinfo/' + this.value, function(data) {
 		  $(obj).replaceWith(data['markup']);
+                  $('.p2b_preloader').remove();
 		});		
     });
 });
