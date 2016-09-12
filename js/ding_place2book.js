2e40a8b487d0425d9c78a7d82d4f38457685cb71
Tobias F. Petersen <tobsf@hotmail.com>
2016-09-12 08:31:18 +0200

    style

--- js/ding_place2book.js
+++ js/ding_place2book.js
@@ -6,7 +6,7 @@
 jQuery(document).ready(function($) {
 	$('.place2book-ticketinfo').each(function() { 
 	    var obj = this;
-            $(obj).parent().append("<div class='p2b_preloader'><i class='fa fa-cog fa-spin fa-2x fa-fw'></i><div>");
+            $(obj).parent().append("<div class='p2b_preloader'><a href='https://place2book.com/da/14540/list/EM3892'>Bestil billet(ter)</a><i class='fa fa-cog fa-spin fa-2x fa-fw'></i><div>");
 		$.getJSON(Drupal.settings.basePath + 'ding/place2book/ticketinfo/' + this.value, function(data) {
 		  $(obj).replaceWith(data['markup']);
                   $('.p2b_preloader').remove();
